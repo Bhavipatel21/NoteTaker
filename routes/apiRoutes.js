b3284e86f8da328ad795cf8313dbb867ea32ae6d
Bhavika Patel <bhavij28@gmail.com>
2021-03-15 16:51:12 -0400

    added delete route to delete notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 // LOAD DATA
 const fs = require("fs");
 const path = require("path");
+const uniqid = require('uniqid');
 let notes = require("../db/db.json");
 
 // ROUTING
@@ -15,9 +16,13 @@ app.get('/api/notes', (req, res) => {
 
 //Add data to db.json
  app.post('/api/notes', (req, res) => {
-        const newNote = req.body;
+        let newNote = {
+            id : uniqid(),
+            title:req.body.title,
+            text : req.body.text
+             }
         console.log(newNote);
-
+       
         notes.push(newNote)
         fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify(notes),(err,data)=> {
 
@@ -27,6 +32,24 @@ app.get('/api/notes', (req, res) => {
     });
 
 //Delete data from db.json
+app.delete('/api/notes/:id', function (req, res) {
+    let targetId = (req.params.id);
 
+    fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err) => {
+      if (err) throw err;          
+      });
+
+    console.log(`Deleting note with id ${targetId}`);        
+    notes = notes.filter(newNote => {
+      return newNote.id !== targetId;
+    
+    })
+    console.log(notes); 
+    
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json({ok: true})
+    });        
+})
 
 };
