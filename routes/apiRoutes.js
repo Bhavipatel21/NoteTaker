3636b32c556e6a2b0f05a508df5a9465fee09e1f
Bhavika Patel <bhavij28@gmail.com>
2021-03-15 16:52:03 -0400

    updated format

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,48 +8,48 @@ let notes = require("../db/db.json");
 
 module.exports = (app) => {
 
-//Display data from db.json
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-   
-});
+    //Display data from db.json
+    app.get('/api/notes', (req, res) => {
+        res.json(notes)
 
-//Add data to db.json
- app.post('/api/notes', (req, res) => {
+    });
+
+    //Add data to db.json
+    app.post('/api/notes', (req, res) => {
         let newNote = {
-            id : uniqid(),
-            title:req.body.title,
-            text : req.body.text
-             }
+            id: uniqid(),
+            title: req.body.title,
+            text: req.body.text
+        }
         console.log(newNote);
-       
-        notes.push(newNote)
-        fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify(notes),(err,data)=> {
 
-            if(err) throw err;
+        notes.push(newNote)
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err, data) => {
+
+            if (err) throw err;
             return res.json(notes);
         })
     });
 
-//Delete data from db.json
-app.delete('/api/notes/:id', function (req, res) {
-    let targetId = (req.params.id);
+    //Delete data from db.json
+    app.delete('/api/notes/:id', function (req, res) {
+        let targetId = (req.params.id);
 
-    fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err) => {
-      if (err) throw err;          
-      });
+        fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err) => {
+            if (err) throw err;
+        });
 
-    console.log(`Deleting note with id ${targetId}`);        
-    notes = notes.filter(newNote => {
-      return newNote.id !== targetId;
-    
+        console.log(`Deleting note with id ${targetId}`);
+        notes = notes.filter(newNote => {
+            return newNote.id !== targetId;
+
+        })
+        console.log(notes);
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json({ ok: true })
+        });
     })
-    console.log(notes); 
-    
-    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.json({ok: true})
-    });        
-})
 
 };
